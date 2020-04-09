00d6f533c3f3a0cb34746e976411c81bbd388eed
Stephen Fang <safang99@gmail.com>
2020-04-09 14:17:41 -0400

    fixed the numericality validation

--- app/views/models/book.rb
+++ app/views/models/book.rb
@@ -2,8 +2,8 @@ class Book < ActiveRecord::Base
     validates :title, presence: true
     validates :author, presence: true
     # validates :favorite, inclusion: { in:[true, false] }
-    validates :rating, numericality: { greater_than_or_equal: 0, less_than_or_equal: 100 }, allow_nil: true
-    
+    validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_nil: true
+
     has_many :checkouts
     has_many :categorizations
     has_many :categories, through: :categorizations
